2186dc1be8fe0f13356fa73aed98e49942d911c2
ekaterinabolotova <bolotova-katya@yandex.ru>
2019-09-18 21:32:42 +0200

    last task for BigInteger exercises is added

--- src/BigIntergerExercises.java
+++ src/BigIntergerExercises.java
@@ -3,9 +3,60 @@ import java.util.Scanner;
 
 public class BigIntergerExercises {
 
-
     public static void main(String[] args) {
-        //Big Integer expression
+
+        Scanner scan = new Scanner(System.in);
+        BigInteger factorial = scan.nextBigInteger();
+        BigInteger newFactorial = BigInteger.valueOf(1);
+
+        BigInteger count=BigInteger.valueOf(1);
+
+        while(newFactorial.compareTo(factorial)==-1){
+            count = count.add(BigInteger.ONE);
+            newFactorial = newFactorial.multiply(count);
+
+        }
+
+        System.out.println(count);
+
+    }
+
+    //   Double factorial
+    /**
+     * Implement a function to compute the double factorial, that is the product of natural numbers with the same parity and not exceeding a given number.
+     *
+     * For example:
+     * 7!!=7⋅5⋅3⋅1
+     * 8!!=8⋅6⋅4⋅2
+     *
+     * The function argument can be any non-negative integer.
+     *
+     * Use BigInteger to solve the problem.
+     *
+     * Sample Input 1:
+     * 7
+     * Sample Output 1:
+     * 105
+     */
+
+    public static BigInteger calcDoubleFactorial(int n) {
+        // type your java code here
+        BigInteger doubleFact = BigInteger.valueOf(1);
+        if (n % 2 > 0) {
+            for (; n > 0; n = n - 2) {
+                doubleFact = doubleFact.multiply(BigInteger.valueOf(n));
+            }
+            return doubleFact;
+        } else {
+            for (; n > 1; n = n - 2) {
+                doubleFact = doubleFact.multiply(BigInteger.valueOf(n));
+            }
+            return doubleFact;
+        }
+    }
+
+    public static void main1(String[] args) {
+        //    An expression with large numbers
         /**
          * Write a program that reads four large integers a, b, c, d and outputs the result of the expression
          *
