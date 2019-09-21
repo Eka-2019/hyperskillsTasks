116d35ced521d21392e4c05a31df5829f534037f
ekaterinabolotova <bolotova-katya@yandex.ru>
2019-09-21 12:22:56 +0200

    ConditionalStatement and Logging exercises are added

---
+++ src/StandardLoggerExercices.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class StandardLoggerExercices {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] set = scan.nextLine().split(" ");
+        int result = 0;
+
+        for (int i = 0; i < set.length; i++) {
+            if (set[i].equals("severe")) {
+                result = result + 1000;
+            }
+            if (set[i].equals("warning")) {
+                result = result + 900;
+            }
+            if (set[i].equals("info")) {
+                result = result + 800;
+            }
+            if (set[i].equals("config")) {
+                result = result + 700;
+            }
+            if (set[i].equals("fine")) {
+                result = result + 500;
+            }
+            if (set[i].equals("finer")) {
+                result = result + 400;
+            }
+            if (set[i].equals("finest")) {
+                result = result + 300;
+            }
+        }
+        System.out.println(result);
+
+    }
+}
